c565c39f9e2bb2f63f44a383f4fb66ba64d0b9c0
Budin Kolyandov <budin_kolyandov@abv.bg>
2020-07-26 18:33:31 +0300

    Started a HackerRank Repository.

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            var ar = Console.ReadLine().ToUpper();
+
+            var position = 0;
+
+            var count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var path = ar[i];
+                if (path == 'U')
+                {
+                    if (++position == 0)
+                    {
+                        count++;
+                    }
+                }
+                else if (path == 'D')
+                {
+                    position--;
+                }
+
+
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ SockMerchant/SockMerchant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SockMerchant
+{
+    class SockMerchant
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var ar = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var count = 0;
+
+            var arGroup = ar.GroupBy(x => x).ToArray();
+
+            for (int i = 0; i < arGroup.Length; i++)
+            {
+                count += (arGroup[i].Count() / 2);
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
